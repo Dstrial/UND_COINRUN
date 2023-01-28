4e06eb2be18fd5ba4a96eaaa6e16f27b92e07276
Hyeong1204 <cho4870@naver.com>
2023-01-28 14:19:52 +0900

    산 배경 - 플립효과 추가

--- COINRUN/Assets/Script/Map/Mountain.cs
+++ COINRUN/Assets/Script/Map/Mountain.cs
@@ -7,15 +7,20 @@ public class Mountain : MonoBehaviour
 {
     public float widthSize = 10.24f;
     public float moveSpeed = 1.5f;
+    public bool isFilpX = false;
+
     Transform[] mountains;
+    SpriteRenderer[] mountainSprites;
 
     private void Awake()
     {
         mountains = new Transform[transform.childCount];
+        mountainSprites = new SpriteRenderer[transform.childCount];
 
         for (int i = 0; i < transform.childCount; i++)
         {
             mountains[i] = transform.GetChild(i);
+            mountainSprites[i] = mountains[i].GetComponent<SpriteRenderer>();
         }
     }
 
@@ -28,6 +33,23 @@ public class Mountain : MonoBehaviour
             if (mountain.position.x < -30.0f)      // x가 -12보다 작아지면
             {
                 mountain.Translate(widthSize * mountains.Length * Vector2.right);                       // 특정 위치에 도착하면 오른쪽으로 이동
+            }
+        }
+
+        for (int i = 0; i < mountains.Length; i++)
+        {
+            mountains[i].Translate(Time.deltaTime * moveSpeed * Vector2.left);       // 계속 왼쪽으로 가게하기
+
+            if (mountains[i].position.x < -30.0f)      // x가 -12보다 작아지면
+            {
+                mountains[i].Translate(widthSize * mountains.Length * Vector2.right);                       // 특정 위치에 도착하면 오른쪽으로 이동
+
+                if (isFilpX)
+                {
+                    int rand = Random.Range(0, 2);
+
+                    mountainSprites[i].flipX = rand != 0;
+                }
             }
         }
     }
