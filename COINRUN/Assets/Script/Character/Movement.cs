4a88120a5f21e365797cfbbd234e5b2b3e53a663
Dstrial <wnstjr0103@Naver.com>
2023-01-31 13:34:43 +0900

    JUMP

--- COINRUN/Assets/Script/Character/Movement.cs
+++ COINRUN/Assets/Script/Character/Movement.cs
@@ -6,16 +6,26 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
     // Start is called before the first frame update
     float moveSpeed;
+    float time;
+    int jmpCount;
     Rigidbody2D rigid;
     void Start() {
+        jmpCount = 0;
         moveSpeed = 10f;
         rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetMouseButtonDown(0)) {
-            rigid.velocity = new Vector2(0, moveSpeed);
+
+        if (jmpCount < 2) {
+            if (Input.GetMouseButtonDown(0)) {
+                rigid.velocity = new Vector2(0, moveSpeed);
+                jmpCount++;
+            }
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision) {
+        jmpCount = 0;
     }
 }
