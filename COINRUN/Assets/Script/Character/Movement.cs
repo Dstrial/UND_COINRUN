d2217649ae3e892985dfae513c9c0e51924c39c9
Hyeong1204 <cho4870@naver.com>
2023-01-31 16:34:48 +0900

    코인 생성 및 오브젝트 풀

--- COINRUN/Assets/Script/Character/Movement.cs
+++ COINRUN/Assets/Script/Character/Movement.cs
@@ -3,29 +3,40 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
     // Start is called before the first frame update
     float moveSpeed;
     float time;
     int jmpCount;
     Rigidbody2D rigid;
-    void Start() {
+    void Start()
+    {
         jmpCount = 0;
-        moveSpeed = 10f;
+        moveSpeed = 7f;
         rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update() {
-
-        if (jmpCount < 2) {
-            if (Input.GetMouseButtonDown(0)) {
+    void Update()
+    {
+        if (jmpCount < 2)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
                 rigid.velocity = new Vector2(0, moveSpeed);
                 jmpCount++;
             }
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision) {
-        jmpCount = 0;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if (rigid.velocity.y < 0.1f)            // 낙하중일 때만 체크
+            {
+                jmpCount = 0;  
+            }
+        }
     }
 }
---
+++ COINRUN/Assets/Script/Map/Coins.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour
+{
+    public bool testOn = false;
+
+    string coinCode;
+    Transform[] coins;
+    int coinCount = 0;
+
+    public float groundSpeed = 5.0f;       // 기본 스피드
+    const float WIDTH = 2.525f;            // 그림 가로 크기
+    float currentSpeed = 1.0f;             // 지속적으로 스피드 증가
+
+    float coinHeihtOffset = 2.7f;
+    Vector2 firstDir = Vector2.zero;
+
+    MapManager mapData;
+
+    private void Awake()
+    {
+        mapData = new MapManager();
+        coinCode = mapData.map.coin;
+
+        coins = new Transform[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            coins[i] = transform.GetChild(i);
+            if (i == 0)
+            {
+                firstDir.y = coins[0].transform.position.y; 
+            }
+
+            BlockSeting(coins[i]);
+        }
+
+    }
+
+    private void Update()
+    {
+        foreach (var ground in coins)
+        {
+            ground.Translate(Time.deltaTime * groundSpeed * currentSpeed * Vector2.left);       // 계속 왼쪽으로 가게하기
+
+            if (ground.position.x < -12.0f)      // x가 -12보다 작아지면
+            {
+                BlockSeting(ground);           // 블럭 다시 설정
+                ground.Translate(WIDTH * coins.Length * Vector2.right);                       // 특정 위치에 도착하면 오른쪽으로 이동
+            }
+        }
+    }
+
+    void BlockSeting(Transform ground)
+    {
+        if (!testOn)            // 테스트 모드가 아닐때만 실행
+        {
+            if (coinCount < coinCode.Length)
+            {
+                Vector3 temp = ground.position;
+                temp.y = firstDir.y;
+                ground.position = temp;
+
+                if (coinCode[coinCount] == '0')
+                {
+                    ground.gameObject.SetActive(false);
+                }
+                else if (coinCode[coinCount] == '1')
+                {
+                    ground.gameObject.SetActive(true);
+                }
+                else if (coinCode[coinCount] == '2')
+                {
+                    ground.Translate(coinHeihtOffset * Vector2.up);
+                    ground.gameObject.SetActive(true);
+                }
+                else if (coinCode[coinCount] == '3')
+                {
+                    ground.Translate(coinHeihtOffset * 2.0f * Vector2.up);
+                    ground.gameObject.SetActive(true);
+                }
+
+                coinCount++;
+            }
+        }
+    }
+}
