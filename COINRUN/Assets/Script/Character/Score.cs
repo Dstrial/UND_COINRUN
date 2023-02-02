9b351364c0b6d09bd0500dd852a2c76166c3f2ef
Dstrial <wnstjr0103@Naver.com>
2023-02-02 14:56:34 +0900

    점프, 스코어

---
+++ COINRUN/Assets/Script/Character/Score.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+    float score;
+    float mapSpeed;
+    void Start() {
+        mapSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        score += 100 * mapSpeed * Time.deltaTime;
+        Debug.Log(score);
+    }
+}
